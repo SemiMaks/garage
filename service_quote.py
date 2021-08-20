971a79387633f9742f23f5e123315a1b067f3fd3
Maksim <semimaks@mail.ru>
2021-08-20 16:14:34 +0300

    Создан класс ServiceQuote

--- service_quote.py
+++ service_quote.py
@@ -1,26 +1,27 @@
 # Константа для ставки налога.
 TAX_RATE = 0.05
 
+
 # Класс ServiceQuote.
 class ServiceQuote:
-    def __init__(self, parts_charges, labor_charges):
-        self.__parts_charges = parts_charges
-        self.__labor_charges = labor_charges
+    def __init__(self, pcharge, lcharge):
+        self.__parts_charges = pcharge
+        self.__labor_charges = lcharge
 
-    def set_parts_charges(self, parts_charges):
-        self.__set_parts_charges = parts_charges
+    def set_parts_charges(self, pcharge):
+        self._parts_charges = pcharge
 
-    def set_labor_charges(self, labor_charges):
-        self.__labor_charges = labor_charges
+    def set_labor_charges(self, lcharge):
+        self.__labor_charges = lcharge
 
     def get_parts_charges(self):
-        return self.__get_parts_charges
+        return self.__parts_charges
 
     def get_labor_charges(self):
         return self.__labor_charges
 
     def get_sales_tax(self):
-        return self.__get_sales_tax
+        return __parts_charges * TAX_RATE
 
     def get_total_charges(self):
-        return self.__get_total_charges
+        return __parts_charges + __labor_charges + (__parts_charges * TAX_RATE)
